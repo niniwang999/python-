177b1063eee98a9b5516923edd40618225defd8b
niniwang999 <niniwang999@gmail.com>
2022-04-16 14:36:10 -0400

    Update pig.py
    ver 0.1

--- pig.py
+++ pig.py
@@ -1,10 +1,13 @@
-import random 
+# this is a python code that runs a dice game called pig. for school hw 
+#ver. 0.1 2022 april 16 
+# by jessie wang, niniwang999@gmail.com 
+import random #it imports a random function 
 
 score = 0
 WINNING_SCORE = 20
-while score < WINNING_SCORE:
-    roll = random.randint (1,6)
-    print ("You rolled a", roll)
+while score < WINNING_SCORE: #a loop that makes it so that hen your score is less than the winning score, which you detrimeint aboeve, it does stuff. 
+    roll = random.randint (1,6) # this is defining the variable roll which makes it choose a random integer between 1 and 6 inclusive. 
+    print ("You rolled a", roll) # after you roll the number gets printed. 
 
     if roll == 1:
         print ("You Lose") 
